91d7e65519a7295471cf567e95c1118329c3b4c4
Jason Mikho <jason_mikho@icloud.com>
2025-06-30 15:38:34 -0400

    Prepare backend for Render deployment

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBalPkxTJp2lFYA6eDP8aC3XtJ22lTFa8E",
-  authDomain: "ai-task-manager-40276.firebaseapp.com",
-  projectId: "ai-task-manager-40276",
-  storageBucket: "ai-task-manager-40276.appspot.com",
-  messagingSenderId: "1047605916089",
-  appId: "1:1047605916089:web:4359d6518e13dba9e0c668",
-  measurementId: "G-R9ML8F0T07"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
