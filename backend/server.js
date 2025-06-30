21dbdb294780b4580e5a28d1539f7c96d15b7dda
Jason Mikho <jason_mikho@icloud.com>
2025-06-30 16:14:57 -0400

    Fixed Reset Password api issue

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@ require('dotenv').config();
 const axios = require('axios');
 const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
+const resetRoutes = require('./resetRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api', resetRoutes);
 
 // 🔐 Initialize Firebase Admin
 if (!admin.apps.length) {
